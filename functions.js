8679c9ebe3b29a6201d0cceedf2d7337d7eb31cf
muradfazliniyaz <107277134+muradfazliniyaz@users.noreply.github.com>
2022-11-22 09:39:50 +0100

    22.11.2022

--- functions.js
+++ functions.js
@@ -1,10 +1,31 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (oddNumber) => {
+  let counter = 0;
+  for (let index = 0; index < oddNumber.length; index++) {
+    if(oddNumber[index] % 2 === 1) {
+      counter++;
+    }
+  }
+  return counter;
+};
 
-const getIndexOfFalseItem = () => {};
+const getIndexOfFalseItem = (falseItem) => {
+  return falseItem;
+};
 
-const filterNumbers = () => {};
+const filterNumbers = (fnumber) => {
+  let fnumber = 23;
+  for (let index = 0; index < fnummer.length; index++) {
+    if (fnumber[index]) {
+      return true;
+    }
+    
+  }
+  return false;
+};
 
-const generateEmailAddress = () => {};
+const generateEmailAddress = (generateEmailAddress) => {
+  let toLowerCase = generateEmailAddress.toLowerCase();
+};
 
 export {
   findTheNumberOfOddNumbers,
@@ -12,3 +33,6 @@ export {
   filterNumbers,
   generateEmailAddress,
 };
+
+
+
